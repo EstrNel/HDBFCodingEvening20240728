413ce32e46bd6486aa01090f0ec8ddfb82f00346
EstrNel <157692456+EstrNel@users.noreply.github.com>
2024-07-30 23:34:44 +0200

    Last try for now

--- chat.js
+++ chat.js
@@ -1,27 +1,27 @@
-// Just starting ...
 // Esther (Nel) Louw
+// Something came up and was not able to start on time
 
 let chatList = [];
 let userList = [];
 let currentUser = "";
 let currentChat = "";
-let currentPos = 0;
+
+//Make use of session storage to store the values.
+//sessionStorage.setItem("ssChatList", chatList);
+//sessionStorage.setItem("ssUserList", userList);
 
 // Add Event listeners for each button
-
-buttonInitChat.addEventListener("click", initChat);
 buttonSetUser.addEventListener("click", setUser);
-x.addEventListener("click", displayUsers);
 buttonEnterChat.addEventListener("click", enterChat);
 
-//Initiate chat
-// Clear previous chats
-function initChat() {
-  let chatList = [];
-  let userList = [];
+//Resume chat
+function resumeChat() {
   let currentUser = "";
   let currentChat = "";
-  let currentPos = 0;
+  let chatList = sessionStorage.getItem("ssChatList");
+  let userList = sessionStorage.getItem("ssUserList");
+  displayChat();
+  displayUser();
 }
 
 //Set current user
@@ -34,27 +34,33 @@ function setUser() {
     console.log(`Found ${currentUser}}`);
   } else {
     userList.push(currentUser);
+    sessionStorage.setItem("ssUserList", userList);
   }
+  displayUser();
 }
 
 function checkUser(user) {
   return user == currentUser;
 }
 
-//Display users
-function displayUsers() {
+//display users
+function displayUser() {
+  document.getElementById("showCurrent").innerHTML = currentUser;
   console.table(userList);
+  document.getElementById("showUsers").innerHTML = userList;
 }
 
 //Enter chat text
 function enterChat() {
   currentChat = prompt("Enter Chat");
   console.log(currentChat);
-  chatList.push(currentUser + ":" + currentChat);
+  chatList.push(currentUser + ":" + currentChat + "<br>");
+  sessionStorage.setItem("ssChatList", chatList);
   displayChat();
 }
 
 //display chats
 function displayChat() {
   console.table(chatList);
+  document.getElementById("showChat").innerHTML = chatList; //JSON.stringify(chatList);
 }
